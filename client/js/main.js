a563d10886ab69b0e0c66c2d4b6baa17b2f2023d
Digoco56 <gueparddgc@gmail.com>
2025-05-07 18:25:41 +0200

    9th commit: admin button

--- client/js/main.js
+++ client/js/main.js
@@ -1,30 +1,31 @@
 const API_BASE = 'https://osu-auction.onrender.com';
 
 fetch(`${API_BASE}/api/user`)
-.then(response => {
-  if (!response.ok) {
-    window.location.href = '/';
-  }
-  return response.json();
-})
-.then(user => {
-  const userInfoDiv = document.getElementById('user-info');
-  userInfoDiv.innerHTML = `
+  .then(response => {
+    if (!response.ok) {
+      window.location.href = '/';
+    }
+    return response.json();
+  })
+  .then(user => {
+    console.log('Usuario cargado:', user);
+    const userInfoDiv = document.getElementById('user-info');
+    userInfoDiv.innerHTML = `
     <p><strong>Username:</strong> ${user.username}</p>
     <p><strong>ID:</strong> ${user.id}</p>
     <img src="${user.avatar_url}" alt="Avatar">
   `;
 
-  if (user.role === 'admin') {
-    console.log("admin entra");
-    const adminBtn = document.createElement('a');
-    adminBtn.href = '/admin.html';
-    adminBtn.className = 'button';
-    adminBtn.textContent = 'Admin Panel';
-    document.body.appendChild(adminBtn);
-  }
-})
-.catch(error => {
-  console.error('Error fetching user info:', error);
-  window.location.href = '/';
-});
+    if (user.role === 'admin') {
+      console.log("admin entra");
+      const adminBtn = document.createElement('a');
+      adminBtn.href = '/admin.html';
+      adminBtn.className = 'button';
+      adminBtn.textContent = 'Admin Panel';
+      document.body.appendChild(adminBtn);
+    }
+  })
+  .catch(error => {
+    console.error('Error fetching user info:', error);
+    window.location.href = '/';
+  });
